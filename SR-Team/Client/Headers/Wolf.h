319191c19fc3e404166215a599aaafa9535443e3
JUSIN-20200630H\Administrator <khkyeom91@gmail.com>
2020-11-05 16:04:07 +0900

    wolf - take damage 추가

--- SR-Team/Client/Headers/Wolf.h
+++ SR-Team/Client/Headers/Wolf.h
@@ -53,6 +53,8 @@ public:
 	static  CWolf* Create(LPDIRECT3DDEVICE9 _pDevice);
 	virtual CGameObject * Clone_GameObject(void * _pArg) override;
 
+	virtual HRESULT Take_Damage(const CComponent* _pDamageComp);
+
 
 
 private:
@@ -60,7 +62,7 @@ private:
 	// Add Component
 	//--------------------------------------------------
 	HRESULT Add_Component();
-	
+
 	HRESULT Add_Component_VIBuffer();
 	HRESULT Add_Component_Transform();
 	HRESULT Add_Component_Texture();
@@ -87,12 +89,14 @@ private:
 
 	// 애니메이션
 	HRESULT Update_Anim(_float _fDeltaTime);
+	HRESULT Anim_Reset_Move();
+	HRESULT Anim_Reset_Attack();
 	HRESULT Update_Anim_Move(_float _fDeltaTime);
 	HRESULT Update_Anim_Attack1(_float _fDeltaTime);
 	HRESULT Update_Anim_Attack2(_float _fDeltaTime);
 
 	// 공격체
-	HRESULT Spawn_InstantImpact(const wstring& LayerTag);
+	HRESULT Spawn_Impact();
 
 
 
@@ -115,15 +119,15 @@ private:
 	STATE		m_ePreState = IDLE;
 	STATE		m_eCurState = IDLE;
 
-	_float		m_fFollowDistance = 0.f;
-	_float		m_fAttackDistance = 0.f;
-	_vec3		m_vMoveDirection = {};
+	_vec3		m_vMoveDirection = {};		// 이동할 or 쳐다 볼 방향
+	_float		m_fFollowDistance = 0.f;	// 추적 가능한 거리
+	_float		m_fAttackDistance = 0.f;	// 공격 가능한 거리
 
 
 	//--------------------------------------------------
 	// 공격
 	//--------------------------------------------------
-	_bool		m_bAttack = true;			// 공격 가능 or 불가능
+	_bool		m_bCanAttack = true;		// 공격 가능 or 불가능
 	_float		m_fAttackDelay = 0.f;		// 공격 가능한 딜레이
 	_float		m_fAttackTimer = 0.f;		// 공격 쿨타임 시간 재는용
 
@@ -133,15 +137,15 @@ private:
 	//--------------------------------------------------
 	// 피격
 	//--------------------------------------------------
-	_bool		m_bHurt = true;
-	_float		m_fHurtDelay = 0.f;
-	_float		m_fHurtTimer = 0.f;
+	_bool		m_bCanHurt = true;			// 피격 가능 or 불가능
+	_float		m_fHurtDelay = 0.f;			// 피격 가능한 딜레이
+	_float		m_fHurtTimer = 0.f;			// 피격 쿨타임 시간 재는용
 
 
 	//--------------------------------------------------
 	// 애니메이션
 	//--------------------------------------------------
-	_int		m_iAnimStep  = 0;
+	_int		m_iAnimStep = 0;
 	_float		m_fAnimTimer = 0.f;
 
 };
