2ce8fb18892f6a9ebdbd41370e1055fc2d179b72
JUSIN-20201006T\Administrator <hijin071621@gmail.com>
2020-11-05 16:02:01 +0900

    번개 추가

---
+++ SR-Team/Client/Headers/ThunderBox.h
@@ -0,0 +1,50 @@
+#pragma once
+#ifndef __THUNDER_BOX_H__
+#define  __THUNDER_BOX_H__
+
+#include "GameObject.h"
+
+USING(Engine)
+BEGIN(Client)
+
+class CDamageInfo;
+
+class CThunderBox final : public CGameObject
+{
+private:
+	explicit CThunderBox(LPDIRECT3DDEVICE9 _pDevice);
+	explicit CThunderBox(const CThunderBox& _rOther);
+	virtual ~CThunderBox() = default;
+public:
+	virtual HRESULT Setup_GameObject_Prototype() override;
+	virtual HRESULT Setup_GameObject(void * _pArg) override;
+	virtual _int Update_GameObject(_float _fDeltaTime) override;
+	virtual _int LateUpdate_GameObject(_float _fDeltaTime) override;
+	virtual CGameObject * Clone_GameObject(void * _pArg) override;
+	virtual HRESULT Render_NoneAlpha() override;
+	virtual HRESULT Take_Damage(const CComponent* _pDamageComp) override;
+public:
+	static CThunderBox* Create(LPDIRECT3DDEVICE9 _pDevice);
+	virtual void Free() override;
+private:
+	HRESULT Add_Component();
+	HRESULT Spawn_ThunderStorm(const wstring& LayerTag, _vec3 _vTargetPos);
+private:
+	CVIBuffer*			m_pVIBufferCom = nullptr;
+	CTransform*			m_pTransformCom = nullptr;
+	CTexture*			m_pTextureCom = nullptr;
+	CSphereCollider*	m_pColliderCom = nullptr;
+	CStatus*			m_pStatusCom = nullptr;
+	CDamageInfo*		m_pDmgInfoCom = nullptr;
+
+	_bool				m_bDead = false;
+	_bool				m_bShieldMain_RenderCheck = false;
+	INSTANTIMPACT		m_tInstant = {};
+	_float				m_fMoveTime = 0.f;
+	_float				m_fDeadTime = 0.f;
+};
+
+END
+
+#endif // !__THUNDER_BOX_H__
+
