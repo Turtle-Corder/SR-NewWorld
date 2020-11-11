7e55d147e0fbe471cb4ece70f26b3728fff9ed4d
LEEDS-PC\LeeDS <riorb@daum.net>
2020-11-12 01:59:00 +0900

    스테이지 로딩 막힌거 수정

--- SR-Team/Client/Codes/Scene_Stage3.cpp
+++ SR-Team/Client/Codes/Scene_Stage3.cpp
@@ -36,6 +36,8 @@ HRESULT CScene_Stage3::Setup_Scene()
 		return E_FAIL;
 
 	if(FAILED(Setup_Layer_Skybox(L"Layer_Skybox")))
+		return E_FAIL;
+
 
 	m_pPreLoader = CPreLoader::Create(m_pDevice, SCENE_TOWN);
 	if (nullptr == m_pPreLoader)
